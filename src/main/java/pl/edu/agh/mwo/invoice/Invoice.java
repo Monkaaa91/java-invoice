3c6bf16d55fd403146fb80cbf3ee818c6ff1678a
Monika Mondrzejewska <mmondrzeje@student.agh.edu.pl>
2026-01-11 15:38:00 +0100

    Invoice

--- src/main/java/pl/edu/agh/mwo/invoice/Invoice.java
+++ src/main/java/pl/edu/agh/mwo/invoice/Invoice.java
@@ -1,30 +1,50 @@
 package pl.edu.agh.mwo.invoice;
 
 import java.math.BigDecimal;
-import java.util.Collection;
+import java.util.*;
+import java.util.Map;
 
 import pl.edu.agh.mwo.invoice.product.Product;
 
 public class Invoice {
-    private Collection<Product> products;
+//     private Collection<Product> products = new ArrayList<>();
 
-    public void addProduct(Product product) {
-        // TODO: implement
+     private Map<Product, Integer> products = new HashMap<>();
+
+     public void addProduct(Product product) {
+        this.addProduct(product, 1);
+
     }
 
     public void addProduct(Product product, Integer quantity) {
-        // TODO: implement
+         this.products.put(product, quantity);
+
     }
 
     public BigDecimal getSubtotal() {
-        return null;
+
+        BigDecimal value = BigDecimal.ZERO;
+        for (Product product : this.products.keySet()) {
+            Integer quantity = this.products.get(product);
+            BigDecimal price = product.getPrice();
+            price = price.multiply(BigDecimal.valueOf(quantity));
+            value = value.add(price);
+        }
+        return value;
     }
+
 
     public BigDecimal getTax() {
-        return null;
+
+      BigDecimal tax =  BigDecimal.ZERO;
+         for (Map.Entry<Product, Integer> entry : products.entrySet()) {
+
+         }
+
+    return tax;
     }
 
-    public BigDecimal getTotal() {
-        return null;
-    }
+
+    public BigDecimal getTotal() {return  BigDecimal.ZERO;}
 }
+
